28598c8584b1c8488f79763721122829946092c7
Juancjc <jcjustinianoc@hotmail.com>
2021-09-22 17:16:24 -0500

    column e list string

--- Exercicios/exercicio.dart
+++ Exercicios/exercicio.dart
@@ -1,4 +1,24 @@
+//
+soma(a, b){
+  print(a+b);
+}
+class Produto{
+  String nome;
+  double preco;
+  Produto(String nome, double preco){
+    this.nome =nome;
+    this.preco = preco;
+  }
+}
+///
 main() {
+
+var p1 = new Produto('Caneta', 4.99);
+//p1.nome ='Lapis';
+//p1.preco = 4.59;
+
+print('O produto ${p1.nome} tem preço ${p1.preco}');
+soma(2, 5);
 
 print("Primeiro codigo");
 int a =3;
@@ -6,6 +26,7 @@ double b = 3.1;
 bool estaChovendo = true;
 bool estaFrio = false;
 var c = "Vc é muito legal";
+var conjunto={0,1,2,3,4,5};
 
 print("c");
 print(c is String); // aqui ele realiza pesquisa pra confirmar que c é uma string
@@ -21,6 +42,30 @@ nomes.add('Antonio');
 print(nomes.length);
 print(nomes.elementAt(0));
 print(nomes[5]);
-
+print(conjunto);
+print(conjunto.length);
 ///
+///
+dynamic x='teste';
+x=123;
+x = false;
+print('$x valor de x');
+
+Map<String, double> notasDosAlunos ={
+  'ANA': 9.7,
+  'Bia': 9.2,
+  'Carlos': 7.8,
+   };
+   for(var chave in notasDosAlunos.keys){
+     print('chave =$chave');
+   } 
+   for(var valor in notasDosAlunos.values){
+     print('valor =$valor');
+   }  
+   for(var registro in notasDosAlunos.entries){
+     print('${registro.key} =${registro.value}');
+   } 
+
+
+
 }
--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,72 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
+
+main() => runApp(new PerguntaApp());
+
+
+class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final /*List<String>*/perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Contador :',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      home: Scaffold(   //me permite criar o corpo do app
+        appBar: AppBar( 
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(0)),
+            Text(perguntas[1])
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
