1d9f3334df9fdd155772b1c30753b3c8c590a956
Juancjc <jcjustinianoc@hotmail.com>
2021-09-23 19:29:14 -0500

    variável no botão

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -6,8 +6,10 @@ main() => runApp(new PerguntaApp());
 
 
 class PerguntaApp extends StatelessWidget {
+  var perguntaSelecionada = 0;
   void responder(){
-    print ('Pergunta respondida!');
+    perguntaSelecionada++;
+    print (perguntaSelecionada);
     
   }
   void Function() responderDeNovo(){
@@ -30,25 +32,24 @@ class PerguntaApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(perguntas.elementAt(0)),
+            Text(perguntas[perguntaSelecionada]),
             Text(perguntas[1]),
+            // botão retorna vazio fica inabilitado 
             RaisedButton(
               child: Text('Resposta 1'),
               onPressed: responder,
             ),
             RaisedButton(
               child: Text('Resposta 2'), 
-              onPressed: responderDeNovo(),
+              onPressed: responder,
             ),
              RaisedButton(
               child: Text('Resposta 3'), 
-              onPressed: (){
-                print ('Pergunta respondida #03!');
-              },
+              onPressed: responder,
             ),
              RaisedButton(
               child: Text('Resposta 4'), 
-              onPressed: () => print ('Pergunta respondida #04!'),
+              onPressed: responder,
              
             ),
           ],
